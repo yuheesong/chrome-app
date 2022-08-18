6eb4bf05a0510e6e4d7a38f0db90daf51ada9318
yuheesong <thddbgml345@naver.com>
2022-08-18 12:12:39 +0900

    working css

--- js/background.js
+++ js/background.js
@@ -2,8 +2,16 @@ const images = ["0.jpg", "1.jpg", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+
+/*As learn
 const bgImage = document.createElement("img");
-
 bgImage.src=`img/${chosenImage}`;
-
 document.body.appendChild(bgImage);
+*/
+
+/*
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.height = "100vh";
+document.body.style.backgroundSize = "cover";
+*/
